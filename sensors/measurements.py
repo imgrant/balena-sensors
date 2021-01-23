54ac7cda4b0df0d6d93f8d634c59c9564b7452e5
Ian Grant <ian@iangrant.me>
2021-01-23 21:11:58 +0000

    Add ha_title attribute for more flexible naming in Home Assistant

--- sensors/measurements.py
+++ sensors/measurements.py
@@ -1,14 +1,14 @@
 class Measurement():
 
-  TEMPERATURE         = { 'name': 'temperature',     'units': '°C',   'precision': 3,  'ha_device_class': 'temperature' }
-  PRESSURE            = { 'name': 'pressure',        'units': 'hPa',  'precision': 2,  'ha_device_class': 'pressure'    }
-  HUMIDITY            = { 'name': 'humidity',        'units': '%rh',  'precision': 2,  'ha_device_class': 'humidity'    }
-  PROXIMITY           = { 'name': 'proximity',       'units': None,   'precision': 0,  'ha_device_class': None          }
-  LIGHT               = { 'name': 'light',           'units': 'lx',   'precision': 5,  'ha_device_class': 'illuminance' }
-  AIR_QUALITY         = { 'name': 'iaq',             'units': None,   'precision': 2,  'ha_device_class': None          }
-  IAQ_ACCURACY        = { 'name': 'iaq_accuracy',    'units': None,   'precision': 0,  'ha_device_class': None          }
-  STATIC_AIR_QUALITY  = { 'name': 's_iaq',           'units': None,   'precision': 2,  'ha_device_class': None          }
-  S_IAQ_ACCURACY      = { 'name': 's_iaq_accuracy',  'units': None,   'precision': 0,  'ha_device_class': None          }
+  TEMPERATURE         = { 'name': 'temperature',     'units': '°C',   'precision': 3,  'ha_device_class': 'temperature',  'ha_title': 'Temperature'     }
+  PRESSURE            = { 'name': 'pressure',        'units': 'hPa',  'precision': 2,  'ha_device_class': 'pressure',     'ha_title': 'Pressure'        }
+  HUMIDITY            = { 'name': 'humidity',        'units': '%rh',  'precision': 2,  'ha_device_class': 'humidity',     'ha_title': 'Humidity'        }
+  PROXIMITY           = { 'name': 'proximity',       'units': None,   'precision': 0,  'ha_device_class': None,           'ha_title': 'Proximity'       }
+  LIGHT               = { 'name': 'light',           'units': 'lx',   'precision': 5,  'ha_device_class': 'illuminance',  'ha_title': 'Light'           }
+  AIR_QUALITY         = { 'name': 'iaq',             'units': None,   'precision': 2,  'ha_device_class': None,           'ha_title': 'Air Quality'     }
+  IAQ_ACCURACY        = { 'name': 'iaq_accuracy',    'units': None,   'precision': 0,  'ha_device_class': None,           'ha_title': 'IAQ Accuracy'    }
+  STATIC_AIR_QUALITY  = { 'name': 's_iaq',           'units': None,   'precision': 2,  'ha_device_class': None,           'ha_title': 'Static IAQ'      }
+  S_IAQ_ACCURACY      = { 'name': 's_iaq_accuracy',  'units': None,   'precision': 0,  'ha_device_class': None,           'ha_title': 'S-IAQ Accuracy'  }
   #
   # Accuracy signal is an integer, 0-3, representing the following states:
   #
@@ -21,10 +21,10 @@ class Measurement():
   #  | MEDIUM_ACCURACY  |   2   | Medium accuracy: auto-trimming ongoing                    |
   #  | HIGH_ACCURACY    |   3   | High accuracy                                             |
   #
-  CO2_EQUIV           = { 'name': 'co2_equivalents',         'units': 'ppm',  'precision': 2,  'ha_device_class': None }
-  BVOC_EQUIV          = { 'name': 'breath_voc_equivalents',  'units': 'ppm',  'precision': 2,  'ha_device_class': None }
-  GAS                 = { 'name': 'gas_resistance',          'units': 'Ω',    'precision': 3,  'ha_device_class': None }
-  GAS_PERCENT         = { 'name': 'gas_percentage',          'units': '%',    'precision': 2,  'ha_device_class': None }
+  CO2_EQUIV           = { 'name': 'co2_equivalents',         'units': 'ppm',  'precision': 2,  'ha_device_class': None, 'ha_title': 'CO2'   }
+  BVOC_EQUIV          = { 'name': 'breath_voc_equivalents',  'units': 'ppm',  'precision': 2,  'ha_device_class': None, 'ha_title': 'VOC'   }
+  GAS                 = { 'name': 'gas_resistance',          'units': 'Ω',    'precision': 3,  'ha_device_class': None, 'ha_title': 'Gas'   }
+  GAS_PERCENT         = { 'name': 'gas_percentage',          'units': '%',    'precision': 2,  'ha_device_class': None, 'ha_title': 'Gas %' }
   #  
   # Gas percentage is an alternative indicator for air pollution [%], which rates 
   # the raw gas sensor resistance value based on the individual sensor history:
