e18a8ace48527661c32b9112675b1ed7abddb7c3
Ian Grant <ian@iangrant.me>
2021-02-12 13:22:36 +0000

    Change BME sensors to Adafruit CircuitPython libraries

--- sensors/bme280.py
+++ sensors/bme280.py
@@ -1,21 +1,21 @@
 from typing import Optional
 from datetime import datetime
-try:
-    from smbus import SMBus
-except ImportError:
-    from smbus2 import SMBus
-import bme280 as pimoroni_bme280
+from busio import I2C
+from board import SCL, SDA
+import adafruit_bme280
 from .measurements import Measurement, MeasurementError
 
+I2C_ADDRESSES = [ 0x76, 0x77 ]
 
 def enumerate_sensors():
   sensors = []
-  bus = SMBus(1)
-  for i2c_address in [pimoroni_bme280.I2C_ADDRESS_GND, pimoroni_bme280.I2C_ADDRESS_VCC]:
+  bus = I2C(SCL, SDA)
+  for i2c_address in I2C_ADDRESSES:
     try:
       sensor = bme280(i2c_addr=i2c_address, i2c_dev=bus)
-    except RuntimeError as error:
-      # If no BME280 is found, a RuntimeError is raised
+    except (OSError, ValueError, RuntimeError) as error:
+      # If no device is found, a ValueError is raised;
+      # if the chip ID doesn't match (i.e. it's not a BME280), RuntimeError is raised
       print("Error initialising BME280 sensor at I2C address {:#x}: {}".format(i2c_address, str(error)))
     else:
       print("Found BME280 sensor with ID {} at I2C address {:#x}".format(sensor.id, i2c_address))
@@ -23,7 +23,7 @@ def enumerate_sensors():
   return sensors
 
 
-class bme280(pimoroni_bme280.BME280):
+class bme280(adafruit_bme280.Adafruit_BME280_I2C):
 
   manufacturer = 'Bosch'
   model = 'BME280'
@@ -32,26 +32,39 @@ class bme280(pimoroni_bme280.BME280):
                             Measurement.HUMIDITY]
 
   def __init__(self,
-               i2c_addr:  Optional[int]     = pimoroni_bme280.I2C_ADDRESS_GND,
+               i2c_addr:  Optional[int]     = I2C_ADDRESSES[0],
                i2c_dev:   Optional[object]  = None):
-    super().__init__(i2c_addr=i2c_addr, i2c_dev=i2c_dev)
-    # Attempt to set up the sensor in "forced" mode
-    # In this mode `update_sensor` will trigger
-    # a new reading and wait for the result.
-    # The chip will return to sleep mode when finished.
-    self.setup(mode='forced')
+    super().__init__(i2c=i2c_dev, address=i2c_addr)
+    self._t = None
+    self._p = None
+    self._h = None
 
   def update_sensor(self):
     try:
-      super().update_sensor() # Sets self.temperature, humidity, pressure
-    except RuntimeError as error:
+      self._t = super().temperature
+      self._p = super().pressure
+      self._h = super().humidity
+    except (ValueError, RuntimeError) as error:
       raise MeasurementError(str(error))
     else:
       self.timestamp = datetime.now().isoformat(timespec='seconds')
 
   @property
+  def temperature(self):
+    return self._t
+
+  @property
+  def pressure(self):
+    return self._p
+
+  @property
+  def humidity(self):
+    return self._h
+
+  @property
   def id(self):
     """A unique identifier (serial number) for the device."""
     # See: https://community.bosch-sensortec.com/t5/MEMS-sensors-forum/Unique-IDs-in-Bosch-Sensors/m-p/6020/highlight/true#M62
-    i = self._i2c_dev.read_i2c_block_data(self._i2c_addr,0x83,4)
-    return f'{(((i[3] + (i[2] << 8)) & 0x7fff) << 16) + (i[1] << 8) + i[0]:08x}'
+    i = self._read_register(0x83, 4)
+    serial = (((i[3] + (i[2] << 8)) & 0x7fff) << 16) + (i[1] << 8) + i[0]
+    return serial
