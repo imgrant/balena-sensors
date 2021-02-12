9283f697d1ab519967585e13fc7f602f08ae159f
Ian Grant <ian@iangrant.me>
2021-02-12 13:22:36 +0000

    Change BME sensors to Adafruit CircuitPython libraries

--- sensors/bme680.py
+++ sensors/bme680.py
@@ -1,26 +1,25 @@
 from typing import Optional
 from datetime import datetime
-try:
-    from smbus import SMBus
-except ImportError:
-    from smbus2 import SMBus
-import subprocess, io, json, time
-from threading import Thread
-# Use the Pimoroni BME680 library to ease handling of probing the 
+from busio import I2C
+from board import SCL, SDA
+# Use the Adafruit BME680 library to ease handling of probing the 
 # chip ID, etc., even though we'll use the Bosch BSEC library later 
 # to get access to the IAQ score output directly from the chip.
-import bme680 as pimoroni_bme680
+import adafruit_bme680
+import subprocess, io, json, time
+from threading import Thread
 from .measurements import Measurement, MeasurementError
 
+I2C_ADDRESSES = [ 0x76, 0x77 ]
 
 def enumerate_sensors():
   sensors = []
-  bus = SMBus(1)
-  for i2c_address in [pimoroni_bme680.constants.I2C_ADDR_PRIMARY, pimoroni_bme680.constants.I2C_ADDR_SECONDARY]:
+  bus = I2C(SCL, SDA)
+  for i2c_address in I2C_ADDRESSES:
     try:
       sensor = bme680(i2c_addr=i2c_address, i2c_dev=bus)
-    except (IOError, RuntimeError) as error:
-      # If no device is found, an IOError is raised;
+    except (OSError, ValueError, RuntimeError) as error:
+      # If no device is found, a ValueError is raised;
       # if the chip ID doesn't match (i.e. it's not a BME680), RuntimeError is raised
       print("Error initialising BME680 sensor at I2C address {:#x}: {}".format(i2c_address, str(error)))
     else:
@@ -29,7 +28,7 @@ def enumerate_sensors():
   return sensors
 
 
-class bme680(pimoroni_bme680.BME680):
+class bme680():
 
   manufacturer = 'Bosch'
   model = 'BME680'
@@ -47,17 +46,17 @@ class bme680(pimoroni_bme680.BME680):
   bsec_data = None
 
   def __init__(self,
-               i2c_addr:    Optional[int]     = pimoroni_bme680.constants.I2C_ADDR_PRIMARY,
+               i2c_addr:    Optional[int]     = I2C_ADDRESSES[0],
                i2c_dev:     Optional[object]  = None,
                bsec_cmd:    Optional[str]     = '/opt/bsec/bsec_bme680',
                config_file: Optional[str]     = '/data/bsec/bsec_iaq.config',
                state_file:  Optional[str]     = '/data/bsec/bsec_iaq.state',
                temp_offset: Optional[float]   = 0.0 ):
-    # Call the Pimoroni BME680 class init() — if the sensor at the I2C address
+    # Call the Adafruit BME680 class init() — if the sensor at the I2C address
     # is not a BME680, an error will be raised.
-    super().__init__(i2c_addr=i2c_addr, i2c_device=i2c_dev)
+    self.bme680_i2c = adafruit_bme680.Adafruit_BME680_I2C(i2c=i2c_dev, address=i2c_addr)
     self.bsec_command = [ bsec_cmd,
-                          "--address",  f'0x{i2c_addr:x}',
+                          "--address",  f'{i2c_addr:#x}',
                           "--config",   config_file,
                           "--state",    state_file,
                           "--offset",   str(temp_offset) ]    
@@ -65,14 +64,13 @@ class bme680(pimoroni_bme680.BME680):
     self.bsec_process = Thread(target=self.bsec_capture)
     self.bsec_process.start()
 
-
   @property
   def id(self):
     """A unique identifier (serial number) for the device."""
     # See: https://community.bosch-sensortec.com/t5/MEMS-sensors-forum/Unique-IDs-in-Bosch-Sensors/m-p/6020/highlight/true#M62
-    i = self._i2c.read_i2c_block_data(self.i2c_addr,0x83,4)
-    return f'{(((i[3] + (i[2] << 8)) & 0x7fff) << 16) + (i[1] << 8) + i[0]:08x}'
-
+    i = self.bme680_i2c._read(0x83, 4)
+    serial = (((i[3] + (i[2] << 8)) & 0x7fff) << 16) + (i[1] << 8) + i[0]
+    return serial
 
   def update_sensor(self):
     try:
